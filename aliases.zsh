101ba3c520c8614180aceb5888015470e1c2f294
Martin Rojo <wat.martinrojo@gmail.com>
2023-03-21 11:10:41 +0000

    add project aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,11 @@ alias ls='exa'
 alias sl='exa'
 alias lg='lazygit'
 
+# PROJECTS
+alias dotfiles='cd ~/dotfiles'
+alias krunker='cd ~/code/frvr/game-krunker-frvr'
+alias crucible='cd ~/code/frvr/frvr-crucible'
+
 # FUNCTIONS
 function mkcd() {
     mkdir -p "$1" && cd "$1"
