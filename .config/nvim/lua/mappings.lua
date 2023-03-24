788b778b9db2c6e4d549cc0800302e92cf0a9380
Martin Rojo <wat.martinrojo@gmail.com>
2023-03-24 23:34:15 +0000

    mappings update

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,6 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.keymap.set({"n", "i", "v"}, "<Up>", "<nop>", { noremap = true, silent = true })
+vim.keymap.set({"n", "i", "v"}, "<Down>", "<nop>", { noremap = true, silent = true })
+vim.keymap.set({"n", "i", "v"}, "<Left>", "<nop>", { noremap = true, silent = true })
+vim.keymap.set({"n", "i", "v"}, "<Right>", "<nop>", { noremap = true, silent = true })
+
 -- telescope
 vim.keymap.set("n", "<leader>ff", ':lua require("telescope")<CR> <cmd>Telescope find_files hidden=true<cr>', opts)
 vim.keymap.set("n", "<leader>fg", ':lua require("telescope")<CR> <cmd>Telescope live_grep<cr>', opts)
