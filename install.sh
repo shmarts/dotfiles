40c314d12b0ef755b0f210a308d6c9b1f4ad6617
Martin Rojo <wat.martinrojo@gmail.com>
2023-03-26 13:53:54 +0100

    disable install.sh install dependencies via flag

--- install.sh
+++ install.sh
@@ -8,26 +8,43 @@
 
 DOTFILES=$(pwd -P)
 
-# Brew taps
-brew tap homebrew/cask-fonts
+# Parse arguments
+install_dependencies=1
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -sd|--skip-dependencies)
+      install_dependencies=0
+      ;;
+    *)
+      error "Invalid command line flag $1" >&2
+      exit 1
+      ;;
+  esac
+  shift
+done
 
-# Brew formulae
-brew install exa
-brew install jq
-brew install ripgrep
-brew install starship
-brew install sketchybar
-brew install lazygit
-brew install zsh-autosuggestions
-brew install zsh-syntax-highlighting
-brew install go
-brew install rust
-brew install pnpm
-brew install fnm
-brew install mprocs
-# Brew casks
-brew install --cask font-fira-code-nerd-font
-brew install --cask alacritty
+# Brew installs
+if [[ "$install_dependencies" -eq 1 ]]; then
+  # Brew taps
+  brew tap homebrew/cask-fonts
+  # Brew formulae
+  brew install exa
+  brew install jq
+  brew install ripgrep
+  brew install starship
+  brew install sketchybar
+  brew install lazygit
+  brew install zsh-autosuggestions
+  brew install zsh-syntax-highlighting
+  brew install go
+  brew install rust
+  brew install pnpm
+  brew install fnm
+  brew install mprocs
+  # Brew casks
+  brew install --cask font-fira-code-nerd-font
+  brew install --cask alacritty
+fi
 
 # Make symlinks in homedir
 files=($(git ls-tree --name-only main))
@@ -36,20 +53,20 @@ echo "Changing to the $DOTFILES directory"
 cd $DOTFILES
 echo "...done"
 for file in ${files[@]}; do
-    if [[ "$file" =~ ^(.gitignore|install.sh|macosdefaults.sh|wallpapers)$ ]]; then continue ; fi
-    echo "Creating symlink to $file in home directory."
-    ln -sfn $DOTFILES/$file ~/$file
+  if [[ "$file" =~ ^(.gitignore|install.sh|macosdefaults.sh|wallpapers)$ ]]; then continue ; fi
+  echo "Creating symlink to $file in home directory."
+  ln -sfn $DOTFILES/$file ~/$file
 done
 
+# .env.sh file
 create_env_file () {
-    if test -f "$HOME/.env.sh"; then
-        echo "$HOME/.env.sh file already exists, skipping"
-    else
-        echo "export DOTFILES=$DOTFILES" > $HOME/.env.sh
-        echo 'created ~/.env.sh'
-    fi
+  if test -f "$HOME/.env.sh"; then
+    echo "$HOME/.env.sh file already exists, skipping"
+  else
+    echo "export DOTFILES=$DOTFILES" > $HOME/.env.sh
+    echo 'created ~/.env.sh'
+  fi
 }
-
 create_env_file
 
 echo ''
