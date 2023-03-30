8d0426c2526a894a85b2a5b4dd1134a41936e6c0
Martin Rojo <wat.martinrojo@gmail.com>
2023-03-30 21:05:46 +0100

    mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,10 +3,16 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 
+-- unbind arrow keys
 vim.keymap.set({"n", "i", "v"}, "<Up>", "<nop>", { noremap = true, silent = true })
 vim.keymap.set({"n", "i", "v"}, "<Down>", "<nop>", { noremap = true, silent = true })
 vim.keymap.set({"n", "i", "v"}, "<Left>", "<nop>", { noremap = true, silent = true })
 vim.keymap.set({"n", "i", "v"}, "<Right>", "<nop>", { noremap = true, silent = true })
+
+-- save files
+vim.keymap.set("n", "<C-S>", ":update<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-S>", "<C-C>:update<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-S>", "<C-O>:update<CR>", { noremap = true, silent = true })
 
 -- telescope
 vim.keymap.set("n", "<leader>ff", ':lua require("telescope")<CR> <cmd>Telescope find_files hidden=true<cr>', opts)
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,3 +3,4 @@ vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+
