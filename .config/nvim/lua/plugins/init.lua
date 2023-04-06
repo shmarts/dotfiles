6020b981aa3d672455d9bd1bb227c158f0725598
Martin Rojo <wat.martinrojo@gmail.com>
2023-04-06 23:43:06 +0100

    lualine

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -68,5 +68,16 @@ return require("lazy").setup({
     end
   },
   'folke/which-key.nvim',
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
 })
 
