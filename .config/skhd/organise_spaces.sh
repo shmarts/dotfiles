73f7bc0ea9d0b3f46c99feabd25f8bca9e898551
Martin Rojo <wat.martinrojo@gmail.com>
2023-07-27 23:08:22 +0100

    make organise_spaces resize windows

--- .config/skhd/organise_spaces.sh
+++ .config/skhd/organise_spaces.sh
@@ -31,12 +31,34 @@ sketchybar --trigger space_change --trigger windows_on_spaces
 
 windows="$(yabai -m query --windows)"
 
+maximise_window() {
+  window_id="$1"
+  echo "$window_id"
+  yabai -m window "$window_id" --grid 1:1:0:0:1:1
+}
+
+center_window() {
+  window_id="$1"
+  display="$(yabai -m query --displays --window "$window_id" | jq '.frame')"
+  display_width="$(jq --argjson display "${display}" -nr '$display.w')"
+  display_height="$(jq --argjson display "${display}" -nr '$display.h')"
+
+  new_width="$(jq --argjson display "${display}" -nr '$display.w / 1.3')"
+  new_height="$(jq --argjson display "${display}" -nr '$display.h / 1.2')"
+  yabai -m window "$window_id" --resize "abs:${new_width}:${new_height}"
+
+  new_x=$(echo "$display_width $new_width" | awk '{print ($1 - $2) / 2}')
+  new_y=$(echo "$display_height $new_height" | awk '{print ($1 - $2) / 2}')
+  yabai -m window "$window_id" --move "abs:${new_x}:${new_y}"
+}
+
 move_app_to_space() {
   app_name="$1"
   space="$2"
+  center="$3"
   app_index=0
-  if [ -n "$3" ]; then
-    app_index="$3"
+  if [ -n "$4" ]; then
+    app_index="$4"
   fi
 
   window_id="$(echo "$windows" |
@@ -46,17 +68,25 @@ move_app_to_space() {
     return
   fi
 
+  if [ "$center" -eq 1 ]; then
+    echo "Centering $app_name"
+    center_window "$window_id"
+  else
+    echo "maximising $app_name"
+    maximise_window "$window_id"
+  fi
+
   yabai -m window "$window_id" --space "$space"
 }
 
-move_app_to_space "Arc" 1
-move_app_to_space "Code" 2 0
-move_app_to_space "Code" 3 1
-move_app_to_space "Obsidian" 4
-move_app_to_space "Notes" 4
-move_app_to_space "Slack" 5
-move_app_to_space "Discord" 6
-move_app_to_space "Spotify" 7
-move_app_to_space "Messages" 8
-move_app_to_space "Figma" 9
-move_app_to_space "Notion" 10
+move_app_to_space "Arc" 1 0
+move_app_to_space "Code" 2 0 0
+move_app_to_space "Code" 3 0 1
+move_app_to_space "Obsidian" 4 1
+move_app_to_space "Notes" 4 1
+move_app_to_space "Slack" 5 1
+move_app_to_space "Discord" 6 1
+move_app_to_space "Spotify" 7 1
+move_app_to_space "Messages" 8 1
+move_app_to_space "Figma" 9 0
+move_app_to_space "Notion" 10 1
